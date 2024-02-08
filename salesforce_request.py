2d639a16ce458e92c454b4856a7a0aee525c6d88
JordyMG97 <jordym.ibm@gmail.com>
2024-02-08 15:08:43 -0600

    soap query request

--- salesforce_request.py
+++ salesforce_request.py
@@ -4,6 +4,8 @@ import xml.etree.ElementTree as ET
 #SOAP request URL
 DOMAIN = "https://solera-amer.my.salesforce.com/services/Soap/u/45.0"
  
+ 
+  ##LOGIN
 #Structured XML
 payload = """<?xml version="1.0" encoding="utf-8" ?>
 <env:Envelope xmlns:xsd="http://www.w3.org/2001/XMLSchema"
@@ -29,10 +31,36 @@ response = requests.post(DOMAIN, headers=headers, data=payload)
 root = ET.fromstring(response.text)
 
 namespace = {'soapenv': 'http://schemas.xmlsoap.org/soap/envelope/', 'sf': 'urn:partner.soap.sforce.com'}
-userId_element = root.find(".//sf:userId", namespace)
+userId_element = root.find(".//sf:sessionId", namespace)
 
 userId_value = userId_element.text if userId_element is not None else None
 
 print(userId_value)
 
-print(userId_element)
+  ##Query
+  
+query_String = "SELECT id FROM  lead"
+  
+payload = """<?xml version="1.0" encoding="utf-8" ?>
+<soapenv:Envelope xmlns:soapenv="http://schemas.xmlsoap.org/soap/envelope/"
+    xmlns:urn="urn:partner.soap.sforce.com">
+    <soapenv:Header>
+        <urn:SessionHeader>
+            <urn:sessionId>{}</urn:sessionId>
+        </urn:SessionHeader>
+    </soapenv:Header>
+    <soapenv:Body>
+        <urn:query>
+            <urn:queryString>{}</urn:queryString>
+        </urn:query>
+    </soapenv:Body>
+</soapenv:Envelope>""".format(userId_value, query_String)
+
+headers = {
+    'Content-Type': 'text/xml',
+    'SOAPAction': ''
+    }
+
+response = requests.post(DOMAIN, headers=headers, data=payload)
+
+print(response)
